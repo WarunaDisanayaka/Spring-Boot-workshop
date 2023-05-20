3a9aefcc67e34d6b63ca160478024975ea193b24
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-20 13:34:32 +0530

    UUID added

---
+++ demo/src/main/java/com/example/demo/Sample.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.fasterxml.uuid.Generators;
+
+import java.util.UUID;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        UUID uuid = Generators.timeBasedGenerator().generate();
+        System.out.println(uuid.toString());
+    }
+
+}
